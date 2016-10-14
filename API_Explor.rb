89e6e3abd6daacedd87c44a776ffe33d46bce6e0
Yvonne <tailcloud14916@gmail.com>
2016-10-14 22:05:10 +0800

    add API of airbnb and google map

--- API_Explor.rb
+++ API_Explor.rb
@@ -5,14 +5,25 @@ require 'http'
 credentials = YAML.load(File.read('credentials.yml'))
 response = {}
 results = {}
-
 # Initialize API connection by getting access_token
-#  require client_id and client_secret
-
-feed_response = HTTP.get("https://api.airbnb.com/v2/search_results?client_id=3092nxybyb0otqw18e8nh5nty")
-response[:feed] = feed_response
-feed = JSON.load(feed_response.to_s)['data']
-results[:feed] = feed
+# airbnb search list
+airbnbList_response = HTTP.get(
+  'https://api.airbnb.com/v2/search_results',
+    params: {
+      client_id: credentials[:airbnb_id]
+    })
+response[:airbnblist] = airbnbList_response
+airbnblist = JSON.load(airbnbList_response.to_s)['data']
+results[:airbnblist] = airbnblist
+# google detail information
+googleDetail = HTTP.get(
+  'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise',
+    params: {
+      key: credentials[:googlemap_id]
+    })
+  response[:googledetail] = googleDetail
+  googledetail = JSON.load(googleDetail.to_s)['data']
+  results[:googledetail] = googledetail
 
 File.write('response.yml', response.to_yaml)
 File.write('results.yml', results.to_yaml)
