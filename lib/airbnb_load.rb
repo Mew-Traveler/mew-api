296c0df0d0161f5256791a24989fd3f2c966cbf3
Yvonne <tailcloud14916@gmail.com>
2016-10-15 15:41:01 +0800

    spec works

--- lib/airbnb_load.rb
+++ lib/airbnb_load.rb
@@ -2,19 +2,17 @@ require 'http'
 
 module Load
 	class Airbnb
-		
 		def initialize(client_id:)
 			airbnbList_response = HTTP.get('https://api.airbnb.com/v2/search_results',
 			   	params: {
 			    	client_id: client_id
 			})
-			response[:airbnblist] = airbnbList_response
-			airbnblist = JSON.load(airbnbList_response.to_s)['data']
-			results[:airbnblist] = airbnblist
+			airbnb_load = JSON.load(airbnbList_response.to_s)
+			@airbnb_data = airbnb_load['data']
 		end
 
 		def write
-			File.write('results.yml', results.to_yaml)
+			File.write('./results.yml', @airbnb_data.to_yaml)
 		end
 	end
 end
--- lib/google_load.rb
+++ lib/google_load.rb
@@ -2,19 +2,17 @@ require 'http'
 
 module Load
 	class Google
-
-		def initialize(key:)
+		def initialize(client_id:)
 			googleDetail = HTTP.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise',
 			    params: {
-			      key: key
+			      key: client_id
 			 })
-			response[:googledetail] = googleDetail
-  			googledetail = JSON.load(googleDetail.to_s)['data']
-  			results[:googledetail] = googledetail
+  		google_load = JSON.load(googleDetail.to_s)
+			@google_data = google_load['data']
 		end
 
 		def write
-			File.write('response.yml', response.to_yaml)
+			File.write('response.yml', @google_data.to_yaml)
 		end
 	end
 end
