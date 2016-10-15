ce3d3120e2969a2d34bec1bdac6e764540da9b13
wtlin <wtlin0711@gmail.com>
2016-10-15 13:18:34 -0700

    [Merge] merge first_refactoring

--- spec/load_spec.rb
+++ spec/load_spec.rb
@@ -8,27 +8,27 @@ require './lib/google_load.rb'
 CREDENTIALS = YAML.load(File.read('config/credentials.yml'))
 
 describe 'Load specifications' do
-	it 'should be able to get the data from Airbnb' do
-		airbnb_load = Load::Airbnb.new(
-			client_id: CREDENTIALS[:airbnb_id]
-		)
+  it 'should be able to get the data from Airbnb' do
+    airbnb_load = Load::Airbnb.new(
+      client_id: CREDENTIALS[:airbnb_id]
+    )
 
-		airbnb_load.airbnb_data.length.must_be :>, 0
-	end
+    airbnb_load.airbnb_data.length.must_be :>, 0
+  end
 
-	it 'should be able to get the data from Google' do
-		google_load = Load::Google.new(
-			key: CREDENTIALS[:googlemap_id]
-		)
+  it 'should be able to get the data from Google' do
+    google_load = Load::Google.new(
+      key: CREDENTIALS[:googlemap_id]
+    )
 
-		google_load.google_data.length.must_be :>, 0
-	end
+    google_load.google_data.length.must_be :>, 0
+  end
 
-	it 'should be able to get the neighborhood information' do
-		airbnb_load = Load::Airbnb.new(
-			client_id: CREDENTIALS[:airbnb_id]
-		)
+  it 'should be able to get the neighborhood information' do
+    airbnb_load = Load::Airbnb.new(
+      client_id: CREDENTIALS[:airbnb_id]
+    )
 
-		airbnb_load.getNeighborhood.length.must_be :>, 0
-	end
+    airbnb_load.getNeighborhood.length.must_be :>, 0
+  end
 end
