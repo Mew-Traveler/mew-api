f9ab08887fcd0338523b9f3370152a1d9682e31d
Yvonne <tailcloud14916@gmail.com>
2016-10-23 23:55:42 +0800

    add Coverage report generated for RSpec to /Users/Yvonne/Desktop/NTHU/Master2_1/SOA/tmp/mew-api/coverage. 0.0 / 0.0 LOC (100.0%) covered.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ require 'webmock'
 
 require './lib/airbnb_api.rb'
 require './lib/google_api.rb'
+require 'simplecov'
+SimpleCov.start
 
 FIXTURES_FOLDER = 'spec/fixtures'
 CASSETTES_FOLDER = "#{FIXTURES_FOLDER}/cassettes"
